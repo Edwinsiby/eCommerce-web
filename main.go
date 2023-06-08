6fd18f03d9b52dd2d2207c1354b4709731a99549
Edwin Siby <124251172+Edwinsiby@users.noreply.github.com>
2023-06-08 16:30:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 //	@license.name	Apache 2.0
 //	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 
-//	@host		localhost:8080
+//	@host		www.zogfestiv.store
 //	@BasePath	/
 
 //	@schemes	http
