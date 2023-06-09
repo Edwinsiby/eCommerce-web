36650f21282be4756803c17678602787d0f8cf12
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-06-09 10:13:05 +0530

    Updation-Database env

--- repository/infrastructure/database.go
+++ repository/infrastructure/database.go
@@ -2,17 +2,28 @@ package infrastructre
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"zog/delivery/models"
 	"zog/domain/entity"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+var dsn string
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn = os.Getenv("KEY5")
+
+}
 func ConnectToDB() (*gorm.DB, error) {
-	dsn := "host=database-1.c58tvxhzgzmq.us-east-1.rds.amazonaws.com user=edwin dbname=postgres password=acidisacid port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
