ada5548d6169078c7b24a26f7740bde0e89ab3f9
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-07-06 11:23:46 +0530

    -git-ignore

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -15,6 +15,23 @@ CREATE TABLE users (
     wallet INTEGER,
     permission BOOLEAN NOT NULL DEFAULT TRUE
 );
+CREATE TABLE admins (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    admin_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE
+);
+INSERT INTO admins (created_at, updated_at, admin_name, email, phone, password, role, active)
+VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Edwin', 'admin01@gmail.com', '9048402133', '$2a$10$KV7
+aw2CXyRZ1MeO0haWN2e1Q3FQTQaDQi/3AGT4XAN1LnhcrChoGq', 'master', TRUE);
+
+
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO edwin;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO edwin;
 
